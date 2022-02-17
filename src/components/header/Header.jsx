7a440e18198713a978494e0965375a87003c44d1
jawad <wadja.b@hotmail.fr>
2022-02-17 15:29:24 +0100

    ajout de react router

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,19 @@
 import {React} from "react";
+import { Link } from "react-router-dom";
 import './header.css';
 import argentBanqueLogo from "../../images/argentBankLogo.png"
 
 function Header() {
   return (
     <nav className="main-nav">
-      <a className="main-nav-logo" href="./index.html">
+      <Link className="main-nav-logo" to="/" >
         <img
           className="main-nav-logo-image"
           src={argentBanqueLogo}
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
-      </a>
+      </Link>
       <div>
         <a className="main-nav-item" href="./sign-in.html">
           <i className="fa fa-user-circle"></i>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Route } from "react-router";
 import './style.css';
 import Home from './pages/Home.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
