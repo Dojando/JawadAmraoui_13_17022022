d6ba53ab0a4b8930cdbbe1723ac6698aaeab8b9f
jawad <wadja.b@hotmail.fr>
2022-03-01 15:30:57 +0100

    ajout du nom d'utilisateur de la page profile dans un composant

---
+++ src/components/username/Username.jsx
@@ -0,0 +1,14 @@
+import {React} from "react";
+import './username.css';
+
+function Username() {
+
+  return (
+  <div className="header">
+    <h1>Welcome back<br />Tony Jarvis!</h1>
+    <button className="edit-button">Edit Name</button>
+  </div>
+  );
+}
+
+export default Username;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,16 +2,14 @@ import {React} from "react";
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 import Account from '../components/account/Account';
+import Username from '../components/username/Username';
 
 function Profile() {
   return (
     <div className="container">
       <Header connected={true}/>
       <main className="main bg-dark">
-        <div className="header">
-          <h1>Welcome back<br />Tony Jarvis!</h1>
-          <button className="edit-button">Edit Name</button>
-        </div>
+        <Username />
         <h2 className="sr-only">Accounts</h2>
         <Account />
         <Account />
