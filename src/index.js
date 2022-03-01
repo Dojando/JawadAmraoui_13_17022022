5fe9de2a01d9a487db49851cd69f000c4cab9d61
jawad <wadja.b@hotmail.fr>
2022-03-01 15:14:14 +0100

    creation de la page profile et de ses composants

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Route } from "react-router";
 import './style.css';
 import Home from './pages/Home.jsx';
 import Login from './pages/Login.jsx';
+import Profile from './pages/Profile.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Login/>} />
+        <Route path="/profile" element={<Profile/>} />
       </Routes>
     </Router>
   </React.StrictMode>,
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,21 @@
 import {React} from "react";
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
-import Form from '../components/form/Form';
+import Account from '../components/account/Account';
 
 function Profile() {
   return (
     <div className="container">
       <Header connected={true}/>
       <main className="main bg-dark">
-        <Form />
+        <div className="header">
+          <h1>Welcome back<br />Tony Jarvis!</h1>
+          <button className="edit-button">Edit Name</button>
+        </div>
+        <h2 className="sr-only">Accounts</h2>
+        <Account />
+        <Account />
+        <Account />
       </main>
       <Footer />
     </div>
